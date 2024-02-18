70ca0e5a72900f84654fc76d10fd0b87ad318d85
Ludvik Roubicek <ludvikjakub.roubicek@unicorn.com>
2024-02-18 01:38:26 +0100

    Add springdoc, remove unnecessary dependencies.

--- src/main/java/com/unicorn/rental/dao/car/CarModelDao.java
+++ src/main/java/com/unicorn/rental/dao/car/CarModelDao.java
@@ -2,6 +2,8 @@ package com.unicorn.rental.dao.car;
 
 import com.unicorn.rental.domain.model.car.CarModel;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarModelDao extends CrudRepository<CarModel, Integer> {
 }
